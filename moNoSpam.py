4c73f98c242ebb0327c914910cb1e47a458da584
eshwarne <northeasteditsofficial@gmail.com>
2019-01-01 17:52:26 +0530

    initial data analysis

--- moNoSpam.py
+++ moNoSpam.py
@@ -1,4 +1,4 @@
-import matplotlib;
+from matplotlib.pyplot import plot
 import matplotlib.pyplot as plt;
 import csv;
 from textblob import TextBlob;
@@ -20,8 +20,17 @@ from sklearn.learning_curve import learning_curve;
 message_text = [line.rstrip() for line in open('./dataset/spamData')]
 
 messagesParsed = pandas.read_csv('./dataset/spamData', sep='\t', quoting=csv.QUOTE_NONE, names=["label","featureText"])
+messagesParsed["len"] = messagesParsed['featureText'].map(lambda message : len(message))
+# print(messagesParsed.head())
+# fig1 = messagesParsed.len.plot(kind = 'hist')
+# plt.show()
 
-print(messagesParsed.head())
+print(messagesParsed.len.describe())
+print(list(messagesParsed.featureText[messagesParsed.len == 910]))
 
+messagesParsed.hist(column='len', by='label',bins=50)
+plt.show()
+
+#DATA PREPROCESSING :
     
        
